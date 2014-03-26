dd4edbecef73ef3e9134c1fa903a93ac60ab8d58
Oscarmal <oscarmaldonado_1@hotmail.com>
2014-03-26 15:28:53 -0600

    Sitio con CSS3 Responsivo

---
+++ responsive/site1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sitio Responsivo</title>
+<!–[if lt IE 9]>
+<script src="css3-mediaqueries.js"></script>
+<![endif]–>
+<link rel="stylesheet" type="text/css" href="site1.css" />
+</head>
+<body>
+	<div id="pantalla">
+		<div id="area-trabajo">
+			
+			<div id="cabecera">Cabecera</div>
+			<div id="cuerpo">Cuerpo
+				<div id="menu-izq">
+					<ul>
+						<li>uno</li>
+						<li>dos</li>
+						<li>tres</li>
+					</ul>
+				</div>
+			</div>
+			<div id="pie">Pie</div>
+		</div>
+	</div>
+	<div id="menu-movil">Menu movil</div>
+</body>
+</html>
